d10de6f5960924b0a50ee75df0649fc080a986a0
KelseyCortez <kelscortez@gmail.com>
2020-04-03 14:57:30 -0400

    finished

---
+++ smallex.py
@@ -0,0 +1,16 @@
+# print numbers 0-99
+
+for number in range(100):
+    print(number)
+
+# print even numbers 0-99
+
+for number in range(100):
+    if (number % 2 == 0):
+        print("{0}" .format(number))
+
+#print odd numbers 0-99
+
+for number in range(100):
+    if (number % 2 != 0):
+        print("{0}" .format(number))
